0f0f960550bf307abf8db1d68cbb45520b90467f
Hawi <hawijarso49@gmail.com>
2026-08-07 17:35:41 +0300

    feat: add centralized route name constants for navigation

---
+++ lib/core/routes/route_names.dart
@@ -0,0 +1,10 @@
+/// Centralized route name constants.
+class RouteNames {
+  RouteNames._();
+
+  static const String productList = '/';
+  static const String productDetails = '/product-details';
+  static const String cart = '/cart';
+  static const String login = '/login';
+  static const String profile = '/profile';
+}
